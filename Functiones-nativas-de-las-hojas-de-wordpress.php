8b2c4670a1e78b41afd8e8fad52ac6a9b0fd8c38
Luis Bernal <blacksk81@gmail.com>
2019-08-21 09:24:36 +0200

    Se elimino duplicidad de codigo.
    Se elimino duplicidad de codigo.
    Se elimino duplicidad de codigo.
    Se elimino duplicidad de codigo.

--- Functiones-nativas-de-las-hojas-de-wordpress.php
+++ Functiones-nativas-de-las-hojas-de-wordpress.php
@@ -7,8 +7,6 @@ pagina de wordpress como titulo de la pagina y parrafo de la textarea
 
 
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
-<?php the_title();?>
-<?php the_content(); ?>
 <?php the_title();?> // toman el titulo de la pagina 
 <?php the_content();?> // toman el contenido de textaeres
 <?php the_excerpt();?> // muestra un extrapto del textarea
